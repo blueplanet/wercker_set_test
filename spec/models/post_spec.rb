8d0e1b2e6cb3398bf66c8197aac715f752181be6
blueplanet <erguolinge@gmail.com>
2015-07-16 06:47:15 +0900

    Add spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to respond_to(:title) }
-  it { is_expected.to respond_to(:body) }
   it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
 end
