a66dd04bbef7174a3f9d1c0675a8c4418b0ad8fb
blueplanet <erguolinge@gmail.com>
2015-07-15 20:14:48 +0900

    Add shoulda-matchers

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { is_expected.to respond_to(:title) }
   it { is_expected.to respond_to(:body) }
+  it { is_expected.to validate_presence_of(:title) }
 end
